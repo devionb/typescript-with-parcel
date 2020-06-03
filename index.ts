293f4702be07ea8119202638527edb6b24bf8549
Devion Buchynsky <devionb@bgsu.edu>
2020-06-03 17:23:01 -0400

    added basic types for index ts

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const isOpen: boolean = false;
+
+const myName: string = 'Dev';
+
+const myAge: number = 23.04;
+
+const list: number[] = [0,1,2]; // array type
+
+const me: [string, number, boolean] = ["Dev", 23, false];
+
+enum Job {
+    student, 
+    intern, 
+    WebDev
+}
+const job: Job = Job.student;
+
+const phone: any = "iphone";
+const tablet: any = 1;
+
